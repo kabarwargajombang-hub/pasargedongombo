2ec3800f0058af5dc82b650f5910ed0eacdde690
pasargedongombo <kabarwargajombang@gmail.com>
2026-07-29 23:53:24 +0700

    Menambahkan proteksi login profil toko

--- profil-toko.js
+++ profil-toko.js
@@ -1,6 +1,8 @@
 // profil-toko.js
 
+
 import { auth, db } from "./firebase.js";
+
 
 import {
 onAuthStateChanged
@@ -13,88 +15,155 @@ setDoc
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 
-// cek pengguna login
 
-onAuthStateChanged(auth, (user)=>{
+
+// Proteksi login
+
+let userAktif = null;
+
+
+
+onAuthStateChanged(auth,(user)=>{
 
 
 if(!user){
 
+
 alert("Silakan login terlebih dahulu");
+
 
 window.location.href="login.html";
 
+
+return;
+
+
 }
+
+
+
+userAktif = user;
+
+
 
 });
 
 
 
-// tombol simpan
+
+
+
+
+// Tombol simpan
 
 const tombolSimpan = document.getElementById("simpan");
 
 
-tombolSimpan.addEventListener("click", async ()=>{
+
+if(tombolSimpan){
+
+
+tombolSimpan.addEventListener("click", async()=>{
+
 
 
 const user = auth.currentUser;
 
 
+
 if(!user){
+
 
 alert("Anda belum login");
 
+
+window.location.href="login.html";
+
+
 return;
 
+
 }
+
+
 
 
 
 const dataToko = {
 
 
+
 namaToko:
+
 document.getElementById("namaToko").value,
 
 
+
+
 namaPemilik:
+
 document.getElementById("namaPemilik").value,
 
 
+
+
 whatsapp:
+
 document.getElementById("whatsapp").value,
 
 
+
+
 dusun:
+
 document.getElementById("dusun").value,
 
 
+
+
 rtRw:
+
 document.getElementById("rtRw").value,
 
 
+
+
 desa:
+
 document.getElementById("desa").value,
 
 
+
+
 alamat:
+
 document.getElementById("alamat").value,
 
 
+
+
 deskripsi:
+
 document.getElementById("deskripsi").value,
 
 
+
+
 email:
+
 user.email
+
 
 
 };
 
 
 
+
+
+
 try{
+
 
 
 await setDoc(
@@ -107,18 +176,31 @@ dataToko
 
 
 
+
 alert("Profil toko berhasil disimpan");
+
 
 
 window.location.href="dashboard.html";
 
 
+
 }
+
+
 
 catch(error){
 
 
-alert("Gagal menyimpan: " + error.message);
+
+alert(
+
+"Gagal menyimpan: "
+
++ error.message
+
+);
+
 
 
 }
@@ -126,3 +208,7 @@ alert("Gagal menyimpan: " + error.message);
 
 
 });
+
+
+
+}
