3690469c54d3b7803527cf232592d2dbc1e9017e
pasargedongombo <kabarwargajombang@gmail.com>
2026-07-31 22:38:34 +0700

    Framework v3 - App global navigation

--- app.js
+++ app.js
@@ -1,25 +1,60 @@
 // ==========================================
-// PASAR GEDONGOMBO FRAMEWORK v2
+// PASAR GEDONGOMBO FRAMEWORK v3
 // app.js
 // ==========================================
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    const menuJual = document.getElementById("menuJual");
-    const menuAkun = document.getElementById("menuAkun");
+const menuJual = document.getElementById("menuJual");
+const menuAkun = document.getElementById("menuAkun");
 
-    if(menuJual){
-        menuJual.addEventListener("click", function(e){
-            e.preventDefault();
-            window.location.href = "login.html";
-        });
-    }
+// cek status login
+const statusLogin = localStorage.getItem("statusLogin");
 
-    if(menuAkun){
-        menuAkun.addEventListener("click", function(e){
-            e.preventDefault();
-            window.location.href = "login.html";
-        });
-    }
+// =============================
+// MENU JUAL
+// =============================
+if(menuJual){
+
+menuJual.addEventListener("click", function(e){
+
+e.preventDefault();
+
+if(statusLogin==="login"){
+
+window.location.href="dashboard.html";
+
+}else{
+
+window.location.href="login.html";
+
+}
+
+});
+
+}
+
+// =============================
+// MENU AKUN
+// =============================
+if(menuAkun){
+
+menuAkun.addEventListener("click", function(e){
+
+e.preventDefault();
+
+if(statusLogin==="login"){
+
+window.location.href="dashboard.html";
+
+}else{
+
+window.location.href="login.html";
+
+}
+
+});
+
+}
 
 });
