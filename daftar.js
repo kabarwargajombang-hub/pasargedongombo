1ca3fed5f9815f261c9fb24031ac552ac66d87de
pasargedongombo <kabarwargajombang@gmail.com>
2026-08-01 18:32:03 +0700

    Menambahkan upload foto profil penjual

--- daftar.js
+++ daftar.js
@@ -14,8 +14,72 @@ setDoc
 
 
 const tombolDaftar = document.getElementById("daftar");
+// ==========================
+// FOTO PROFIL
+// ==========================
 
+const fotoProfil = document.getElementById("fotoProfil");
 
+const previewFoto = document.getElementById("previewFoto");
+
+const btnFoto = document.getElementById("btnFoto");
+
+// ==========================
+// PILIH FOTO
+// ==========================
+
+btnFoto.addEventListener("click",()=>{
+
+fotoProfil.click();
+
+});
+
+fotoProfil.addEventListener("change",()=>{
+
+const file = fotoProfil.files[0];
+
+if(file){
+
+previewFoto.src = URL.createObjectURL(file);
+
+}
+
+});
+
+// ==========================
+// UPLOAD CLOUDINARY
+// ==========================
+
+async function uploadFoto(file){
+
+const formData = new FormData();
+
+formData.append("file",file);
+
+formData.append(
+"upload_preset",
+"pasar_gedongombo"
+);
+
+const response = await fetch(
+
+"https://api.cloudinary.com/v1_1/pthqkjlo/image/upload",
+
+{
+
+method:"POST",
+
+body:formData
+
+}
+
+);
+
+const hasil = await response.json();
+
+return hasil.secure_url;
+
+}
 
 tombolDaftar.addEventListener("click", async()=>{
 
@@ -58,20 +122,30 @@ document.getElementById("desa").value;
 if(
 
 namaPemilik=="" ||
+
 namaToko=="" ||
+
 email=="" ||
+
 password=="" ||
+
 whatsapp=="" ||
+
 dusun==""
 
 ){
 
-
 alert("Lengkapi data pendaftaran");
-
 
 return;
 
+}
+
+if(!fotoProfil.files[0]){
+
+alert("Silakan pilih foto profil.");
+
+return;
 
 }
 
@@ -94,7 +168,14 @@ password
 
 
 const user = hasil.user;
+  
+// Upload Foto Profil
 
+const fotoUrl = await uploadFoto(
+
+fotoProfil.files[0]
+
+);
 
 
 
@@ -134,7 +215,9 @@ email:email,
 
 role:"penjual",
 
-deskripsi:""
+deskripsi:"",
+
+fotoProfil:fotoUrl
 
 
 }
